a8443d64ad1ba13b7e0533226d6dd06d9cbb47af
steven_c <steven.e.cutting@gmail.com>
2014-12-25 10:04:58 -0600

    Added timer to rpitests.

--- rpitests.py
+++ rpitests.py
@@ -15,6 +15,7 @@ if __name__ == "__main__":
     import sys
     from decimal import Decimal
 
+    from maths import generalfunctions as gfunc
 
     import maths
     import maths.tests.wallis.wallistests as wtests
@@ -22,18 +23,19 @@ if __name__ == "__main__":
     import logging
     _LOG = logging.getLogger(__name__)
 
+    with gfunc.Timer() as timer:
+        print sys.argv
 
-    print sys.argv
+        i = int(sys.argv[2])
+        arg = sys.argv[1]
 
-    i = int(sys.argv[2])
-    arg = sys.argv[1]
+        if arg == 'std':
+            print 'std:'
+            pie, i = maths.wallis(i)
+            print "\ni = {}\npi = {}".format(i, Decimal(pie))
+        elif arg == 'gen':
+            print 'gen:'
+            pie, i = maths.wallisgen(i)
+            print "\ni = {}\npi = {}".format(i, Decimal(pie))
 
-    if arg == 'std':
-        print 'std:'
-        pie, i = maths.wallis(i)
-        print "\ni = {}\npi = {}".format(i, Decimal(pie))
-    elif arg == 'gen':
-        print 'gen:'
-        pie, i = maths.wallisgen(i)
-        print "\ni = {}\npi = {}".format(i, Decimal(pie))
-
+    print timer.interval
