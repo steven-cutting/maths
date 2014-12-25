e76d8f542ee91779bb4425fb971ba8b6f7881eca
steven_c <steven.e.cutting@gmail.com>
2014-12-25 10:45:56 -0600

    Test bash script.

---
+++ runpie.sh
@@ -0,0 +1,19 @@
+
+time command | python multicore.py 4 1000000000
+time command | python rpitests.py std 1000000000
+
+echo '--'
+echo '--'
+
+time command | python multicore.py 4 1000000000
+time command | python rpitests.py std 1000000
+
+echo '--'
+echo '--'
+
+time command | python multicore.py 4 1000000000
+time command | python rpitests.py std 1000000000
+
+
+
+
